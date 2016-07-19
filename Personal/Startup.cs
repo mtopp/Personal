5c8111d552492873abf64d5135625aa5b3a31ca4
mtopp <michael.topping3@gmail.com>
2016-07-19 12:52:43 +0100

    Blank project

---
+++ Personal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Personal.Startup))]
+namespace Personal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
